94222bdf567dcfb6833d948bd042ed30a7693176
Tomas <tomaslonnblad@gmail.com>
2025-11-11 07:28:44 +0100

    Update HatModel.jsx

--- src/components/models/HatModel.jsx
+++ src/components/models/HatModel.jsx
@@ -8,7 +8,7 @@ import { useFrame } from "@react-three/fiber";
 
 const HatModel = React.memo(function HatModel(props) {
   // Use React.memo for performance optimization
-  const { nodes, materials } = useGLTF("/models/hat-transformed.glb");
+  const { nodes, materials } = useGLTF("/models7/hat-transformed.glb");
 
   const modelRef = useRef();
 
